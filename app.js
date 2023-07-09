cb8ac2dbbe95fd2c4b205fdac56d225433a9af08
Your Name <you@example.com>
2023-07-09 16:21:21 -0700

    learning js

---
+++ app.js
@@ -0,0 +1,84 @@
+// collect user info of car,model and year
+
+// let car = prompt("Enter your car :");
+// let model =prompt("Enter your car model : ");
+// let year = prompt("Enter the year of your car : ")
+
+// function user(){
+//     console.log(`Enter your car : ${car} `)
+//     console.log(`Enter your car model : ${model}`)
+//     console.log(`Enter the year of your car : ${year}`)
+//     alert(` my dream car is ${car},  ${model}, year ${year}`)
+//     console.log(` my dream car is ${car},  ${model}, year ${year}`)
+// }
+// // calling the function
+// user()
+
+
+// let i = 0;
+// while(i < 3){
+//     console.log(i);
+//     i++;
+// }
+// let i = 3;
+// while (i) { // when i becomes 0, the condition becomes falsy, and the loop stops
+//   console.log( i );
+//   i--;
+// }
+
+// let i = 0;
+// do {
+//     console.log( i );
+//   i++;
+// } while (i < 3);
+
+// let p = 2;
+// do{
+//     console.log(p);
+//     p++;
+// }while(p <= 5)
+//  let i = 0;
+
+// for(; i < 3;){
+//     console.log(i++)
+    
+// }
+
+// for(;;){
+//     break;
+// }
+
+// let sum = 0;
+
+// while (true) {
+
+//   let value = + prompt("Enter a number", '');
+
+//   if (!value) break; // (*)
+
+//   sum += value;
+
+// }
+// alert( 'Sum: ' + sum );
+
+// for ( i = 0; i <= 10; i++){
+//   console.log(i);
+
+// }
+// let i = 0;
+// while(i <= 10){
+//   console.log(i);
+//   i++;
+// }
+// for(i = 1; i <= 5; i++){
+  
+//   if(i % 2 !== 0){
+//      console.log(i)
+//   }
+// }
+
+let gender = ["boy","girl","bi-gender"]
+console.log(gender[gender.length - 1]);
+console.log(gender.at(-1))
+// push add at d end
+// pop remove at d end
